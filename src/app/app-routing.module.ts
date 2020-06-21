da06ea6af66c9337276250eaa4aebe7b37da3292
sranmanpreet <manna1661@gmail.com>
2020-06-21 22:27:03 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroComponent } from './landing/hero/hero.component';
+import { PhotosHomeComponent } from './photos/photos-home/photos-home.component';
+import { BlogHomeComponent } from './blog/blog-home/blog-home.component';
+import { ProjectsHomeComponent } from './projects/projects-home/projects-home.component';
+import { CubeComponent } from './projects/animations/cube/cube.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeroComponent },
+  { path: 'home', component: HeroComponent },
+  { path: 'projects', component: ProjectsHomeComponent, children: [
+    {
+      path: 'cube', component: CubeComponent
+    }
+  ]},
+  { path: 'blog', component: BlogHomeComponent },
+  { path: 'photos', component: PhotosHomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { HeroComponent } from './landing/hero/hero.component';
+import { BlogHomeComponent } from './blog/blog-home/blog-home.component';
+import { PhotosHomeComponent } from './photos/photos-home/photos-home.component';
+import { ProjectsHomeComponent } from './projects/projects-home/projects-home.component';
+import { CubeComponent } from './projects/animations/cube/cube.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    BlogHomeComponent,
+    PhotosHomeComponent,
+    ProjectsHomeComponent,
+    CubeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
